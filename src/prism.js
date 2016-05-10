b7107f45156a7acc110ee46ec6879e9fef735112
DeathWish546 <mibo.zhao@hotmail.ca>
2016-05-09 22:04:52 -0400

    Introduced reflect logic to the code, glhf reading this shit charles

--- src/prism.js
+++ src/prism.js
@@ -9,6 +9,27 @@ class Prism {
         this.graphics.beginFill(this.colour, 0.7);
         this.graphics.drawRect(this.position.x * 50, this.position.y * 50, 50, 50);
         this.graphics.endFill();
+
+        if (this.type === Prism.TYPE.REFLECT) {
+            this.graphics.lineStyle(5, 0x000000, 0.5);
+            if (this.direction.x === 0) {
+                console.log("E/W: also, direction: " + this.direction);
+                this.graphics.moveTo(this.position.x * 50, this.position.y * 50 + 25);
+                this.graphics.lineTo(this.position.x * 50 + 50, this.position.y * 50 + 25);
+            } else if (this.direction.y === 0) {
+                console.log("N/S: also, direction: " + this.direction);
+                this.graphics.moveTo(this.position.x * 50 + 25, this.position.y * 50);
+                this.graphics.lineTo(this.position.x * 50 + 25, this.position.y * 50 + 50);
+            } else if (this.direction.x === this.direction.y) {
+                console.log("NE/SW: also, direction: " + this.direction);
+                this.graphics.moveTo(this.position.x * 50 + 50, this.position.y * 50);
+                this.graphics.lineTo(this.position.x * 50, this.position.y * 50 + 50);
+            } else if (this.direction.x === -1 * this.direction.y) {
+                console.log("NW/SE: also, direction: " + this.direction);
+                this.graphics.moveTo(this.position.x * 50, this.position.y * 50);
+                this.graphics.lineTo(this.position.x * 50 + 50, this.position.y * 50 + 50);
+            }
+        }
         stage.addChild(this.graphics);
     }
 
--- src/scene-manager.js
+++ src/scene-manager.js
@@ -4,6 +4,8 @@ let Prism = require('./prism');
 
 let screen = new Screen({width: 800, height: 600, gridLength: 50});
 
+let {DIRECTION, COLOUR} = require('./constants');
+
 class SceneManager {
     constructor() {
         this.beams = [];
@@ -32,7 +34,6 @@ class SceneManager {
             let position = screen.getGridFromPosition(beam.end.x, beam.end.y);
             if (typeof this.occupied[position.x] != 'undefined' && this.occupied[position.x][position.y] && beam.speed > 0) {
                 beam.speed = 0;
-
                 let activatedPrism = this.occupied[position.x][position.y];
                 if (activatedPrism.type === Prism.TYPE.TINT) {
                     this.add.beam(new Beam({
@@ -40,6 +41,26 @@ class SceneManager {
                         y: beam.end.y + (beam.direction.y * 50),
                         colour: activatedPrism.colour,
                         direction: beam.direction
+                    }));
+                //REFLECT Logic
+                } else if (activatedPrism.type === Prism.TYPE.REFLECT && activatedPrism.colour === beam.colour) {
+                    let newBeamDirection = {x: beam.direction.x, y: beam.direction.y};
+                    if ((activatedPrism.direction === DIRECTION.N || activatedPrism.direction === DIRECTION.S) && beam.direction.y !== 0) {
+                        newBeamDirection.y = beam.direction.y * -1;
+                    } else if ((activatedPrism.direction === DIRECTION.E || activatedPrism.direction === DIRECTION.W) && beam.direction.x !== 0) {
+                        newBeamDirection.x = beam.direction.x * -1;
+                    } else if ((activatedPrism.direction === DIRECTION.NE || activatedPrism.direction === DIRECTION.SW) && beam.direction.x !== -1 * beam.direction.y) {
+                        newBeamDirection.x = beam.direction.y;
+                        newBeamDirection.y = beam.direction.x;
+                    } else if ((activatedPrism.direction === DIRECTION.NW || activatedPrism.direction === DIRECTION.SE) && beam.direction.x !== beam.direction.y) {
+                        newBeamDirection.x = beam.direction.y * -1;
+                        newBeamDirection.y = beam.direction.x * -1;
+                    } else { continue; }
+                    this.add.beam(new Beam({
+                        x: (newBeamDirection.x * 25  ) + (position.x * 50) + 25,
+                        y: (newBeamDirection.y * 25  ) + (position.y * 50) + 25,
+                        colour: beam.colour,
+                        direction: newBeamDirection,
                     }));
                 }
             }
