c2af60921dc35963eb492862c9da42914dd66f45
AdamWong <adamwong1000@hotmail.com>
2016-05-12 02:43:59 -0400

    Add test shader code to change line colours

--- src/main.js
+++ src/main.js
@@ -35,6 +35,47 @@ sceneManager.add.prism(new ReflectPrism({ x: 4, y: 3, colour: COLOUR.GREEN, dire
 sceneManager.add.beam(new Beam({ x: 300, y: 375, colour: COLOUR.RED, direction: DIRECTION.E }));
 sceneManager.add.prism(new RefractPrism({ x: 8, y: 7, colour: COLOUR.RED, direction: DIRECTION.NW }));
 
+//Multiple colour line test
+var fragmentShader = [
+    'precision mediump float;',
+    '',
+    'varying vec2 vTextureCoord;',
+    'uniform sampler2D uSampler;',
+    'uniform float delta;',
+    '',
+    'void main(void)',
+    '{',
+    '    vec4 pixel = texture2D(uSampler, vTextureCoord);',
+    '    if (pixel.r > 0.0 || pixel.g > 0.0 || pixel.b > 0.0)',
+    '    {',
+    '        float wave = sin(3.0 * delta);',
+    '        gl_FragColor = mix(vec4(1.0, 0.0, 0.0, 0.0), vec4(0.0, 1.0, 0.0, 0.0), wave);',
+    '    }',
+    '}'
+  ].join('\n');
+
+var graphics = new PIXI.Graphics();
+
+graphics.lineStyle(5, 0xffd900, 1);
+
+// draw a shape
+graphics.moveTo(50,50);
+graphics.lineTo(250, 50);
+
+stage.addChild(graphics);
+
+//Adapted from http://blog.cjgammon.com/custom-filters-with-pixi-js-using-glsl-shaders
+function ColourFilter(fragmentSource)
+{
+    PIXI.AbstractFilter.call(this, null, fragmentShader, { delta: {type:'f', value: 0} } );
+}
+
+ColourFilter.prototype = Object.create(PIXI.AbstractFilter.prototype);
+ColourFilter.prototype.constructor = ColourFilter;
+
+var filter = new ColourFilter();
+graphics.filters = [filter];
+
 // run the render loop
 animate();
 
@@ -43,6 +84,8 @@ function animate() {
 
     var delta = currentTime - previousTime;
     previousTime = currentTime;
+	
+	filter.uniforms.delta.value += delta;
 
     sceneManager.update(delta);
 
